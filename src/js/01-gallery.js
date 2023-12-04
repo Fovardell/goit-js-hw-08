d497db23117ba5c0f952804b569700503956107d
Oleksandr Volokhov <Flatroniym@gmail.com>
2023-12-04 04:47:24 +0100

    виконав перше завдання, підключив бібліотеку на друге завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,28 @@
-/* TODO: ця робота виконується вже за допомогою Парсела, тому уважно читаєм ТЗ і файл readme в шаблоній збірці Саші Репети, щоб правильно звʼязати ваш репозиторій з шаблоном, встановити всі залежності, налаштувати gh-pages і правильно запускати збірку.
-Go Live в цій роботі вже не використовуємо. Для запуска процеса розробки в терміналі використовуємо команду npm start і використовуємо локальний сервер, який створює Парсел для перегляду результату в браузері.
-У кого система Віндовс, не використовуєм термінал powershell. Замінюємо його на bash або git bash
-Усі бібліотеки встановлюємо в проект за допомогою npm. cdn-посилання на бібліотеки не використовуєм.
-В завданні 2 домашньої роботи уважно читаємо ТЗ, щоб правильно використати потрібні методи бібліотеки, а саме метод on() з подією timeupdate і метод setCurrentTime() для встановлення часу після перезавантаження сторінки. Також не забуваємо перевіряти наявність данних, коли читаєте з localStorage. Якщо в localStorage немає ключа, який ви намагаєтесь прочитати, метод getItem(key) поверне вам null.
-В завданні 3 також потрібно перевіряти всі значення, які ви читаєте з localStorage і записуєте в поля форми. Якщо там пусто, вам у поля запишеться undefined. При сабміті форми не забувайте чистити обʼєкт в якому зберігаєте значення з полів форми, щоб інформація не тягнулась в наступні сабміти. Форма має відправлятись при заповнених 2-х полях форми
-Також залишу вам відео-інструкцію, як користуватись Парселем:
-https://www.loom.com/share/88768db322604d6e8e227e590db52f00
-Всім продуктивного тижня :wink:*/
-
-
-
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(i => `		
+		<li class="gallery__item">
+			<a class="gallery__link" href="${i.original}">
+				<img class="gallery__image" src="${i.preview}"
+					alt="${i.description}" />
+			</a>
+		</li>`)
+	.join('');
+
+gallery.innerHTML = markup;
+
+const options = {
+	captions: true,
+	captionType: 'attr',
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+
+};
+var lightbox = new SimpleLightbox('.gallery a', options);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+// /* TODO:
+// В завданні 2 домашньої роботи уважно читаємо ТЗ, щоб правильно використати потрібні методи бібліотеки, а саме метод on() з подією timeupdate і метод setCurrentTime()
+//  для встановлення часу після перезавантаження сторінки. Також не забуваємо перевіряти наявність данних, коли читаєте з localStorage. Якщо в localStorage немає ключа,
+// 	який ви намагаєтесь прочитати, метод getItem(key) поверне вам null.
 
+// 4 Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення. https://github.com/vimeo/player.js/#onevent-string-callback-function-void https://github.com/vimeo/player.js/#events
+// 5 Зберігай час відтворення у локальне сховище.Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// 6 Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції. https://github.com/vimeo/player.js/#setcurrenttimeseconds-number-promisenumber-rangeerrorerror
+// 7 Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.; https://www.npmjs.com/package/lodash.throttle
+
+import Player from '@vimeo/player';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+player.on('play', function () {
+	console.log("played the video");
+});
